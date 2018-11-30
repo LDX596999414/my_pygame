5fea83d9c2bc33c59cfaf83ced4d3dffa2ecd18d
李鼎鑫 <596999414@qq.com>
2018-11-30 21:07:03 +0800

    11.30-2

--- flappybird.py
+++ flappybird.py
@@ -16,40 +16,51 @@ class Bird(object):
         self.gravity = 0     # 重力
         self.dead = False    # 默认小鸟的生命状态为活着
 
-    def bridUpdate(self):
+    def birdUpdate(self):
         if self.jump:
             self.jumpSpeed -= 1            # 速度递减， 上升越来越慢
             self.birdY -= self.jumpSpeed   # 小鸟Y轴坐标减小， 小鸟上升
         else:
             # 小鸟掉落
-            self.gravity = 0.01
+            self.gravity = 1
             self.birdY += self.gravity     # 重力增加 下落越来越快
         self.birdRect[1] = self.birdY      # 更改Y轴位置
 
 
 class Pipeline(object):
     def __init__(self):
-        pass
+        self.catX = 400
+        self.cat = pg.image.load("ali.png")
+
 
     def updatePipeline(self):
-        pass
+        self.catX -= 5                # 小猫向左移动
+        if self.catX < -80:           # 实现cat反复移动
+            self.catX = 400
 
 
 def createMap():
+    """定义创建地图的方法"""
+
     screen.fill((255, 255, 255))       # 填充颜色
     screen.blit(background, (0, 0))    # 填入背景
     # 显示ali
     screen.blit(Bird.birdStatus[0], (Bird.birdX, Bird.birdY))  # 设置ali坐标
+    Bird.birdUpdate()                  # ali 移动
+    # 显示cat
+    screen.blit(Pipeline.cat, (Pipeline.catX, 500))
+    Pipeline.updatePipeline()          # 管道移动
+
     pg.display.update()                # 显示更新
 
 
 if __name__ == "__main__":
-    pg.init()
-    size = width, height = 384, 598
-    screen = pg.display.set_mode(size)
-    clock = pg.time.Clock()
-    Pipeline = Pipeline()
-    Bird = Bird()
+    pg.init()                                 # 初始化 pygame
+    size = width, height = 384, 598           # 设置窗口
+    screen = pg.display.set_mode(size)        # 显示窗口
+    clock = pg.time.Clock()                   # 设置时钟
+    Pipeline = Pipeline()                     # 实例化
+    Bird = Bird()                             # 实例化
     while True:
         clock.tick(60)
         for event in pg.event.get():
